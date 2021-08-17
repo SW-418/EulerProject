8be198dc519877440983afacc5d82de840149311
Sam Wells <samwells95@gmail.com>
2021-08-17 22:17:50 +0100

    Add question 9 - O(n^2)

--- src/C#/Euler/Euler/Program.cs
+++ src/C#/Euler/Euler/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Euler.Question7;
 using Euler.Question8;
+using Euler.Question9;
 
 namespace Euler
 {
@@ -16,6 +17,10 @@ namespace Euler
             // Question 8
             // var result8 = LargestProductInASeries.FindLargestProduct(QuestionInput.Question8Input, 13);
             // Console.WriteLine(result8);
+            
+            //Question 9
+            var result9 = SpecialPythagoreanTriplet.CalculateSpecialTriplet(1000);
+            Console.WriteLine(result9);
         }
     }
 }
